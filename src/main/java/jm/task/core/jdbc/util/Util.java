5d439389fcb65f95a77971a1ca6e483d58c26aad
Leka-SP <Leka-SP@mail.ru>
2020-11-02 21:40:05 +0300

    Commit 02.11.20

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/usertest?serverTimezone=UTC&useSSL=false";
+    private static final String DB_UserName = "root";
+    private static final String DB_Password = "ALEKA678022";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_Driver);
+            connection = DriverManager.getConnection(DB_URL, DB_UserName, DB_Password);
+            System.out.println("Connection succses");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection fatal error");
+        }
+        return connection;
+    }
 }
