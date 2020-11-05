219a45727461fc2787ebebd14c6bcb856c79d437
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2020-11-05 15:08:05 +0300

    Commit 05.11.20

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        Util util = new Util();
-        util.getConnection();
+//        Util util = new Util();
+//        util.getConnection();
+
+        UserServiceImpl userService = new UserServiceImpl();
+        try {
+            userService.createUsersTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,37 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+public class UserServiceImpl extends Util implements UserService {
 
+
+    public void createUsersTable() throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            connection = getConnection();
+            statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE User ("
+                                        + "IdUser INT (6) PRIMARY KEY NOT NULL AUTO_INCREMENT, "
+                                        + "Name VARCHAR (80) NOT NULL, LastName VARCHAR (80) NOT NULL, "
+                                        + "Age FLOAT (4) NOT NULL)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public void dropUsersTable() {
