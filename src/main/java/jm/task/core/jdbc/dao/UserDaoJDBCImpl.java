84cb43c3b9084032d0d64efd290fac2fdd0920ee
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2020-11-05 20:24:48 +0300

    Commit-2 05.11.20

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,16 +1,39 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+public class UserDaoJDBCImpl extends Util implements UserDao {
 
+    public UserDaoJDBCImpl() {
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
 
+        try {
+            connection = getConnection();
+            statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE User ("
+                    + "IdUser INT (6) PRIMARY KEY NOT NULL AUTO_INCREMENT, "
+                    + "Name VARCHAR (80) NOT NULL, LastName VARCHAR (80) NOT NULL, "
+                    + "Age FLOAT (4) NOT NULL)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,37 +1,17 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
+
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
-public class UserServiceImpl extends Util implements UserService {
+public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
 
 
     public void createUsersTable() throws SQLException {
-        Connection connection = null;
-        Statement statement = null;
-
-        try {
-            connection = getConnection();
-            statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE User ("
-                                        + "IdUser INT (6) PRIMARY KEY NOT NULL AUTO_INCREMENT, "
-                                        + "Name VARCHAR (80) NOT NULL, LastName VARCHAR (80) NOT NULL, "
-                                        + "Age FLOAT (4) NOT NULL)");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (statement != null) {
-                statement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-        }
+        super.createUsersTable();
     }
 
     public void dropUsersTable() {
